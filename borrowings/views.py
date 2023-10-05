3192c05060e533a47c332480c72a43d860c83e22
Volodymyr Bohomol <8773927@gmail.com>
2023-10-05 15:04:28 +0300

    - added viewset with list and retrieve functionality

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from borrowings.models import Borrowing
+from borrowings.serializers import BorrowingSerializer
+
+
+class BorrowingViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    GenericViewSet
+):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingSerializer
