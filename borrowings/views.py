93e08d5f97b29f6b3f829d1aba8c566367abdfa0
Volodymyr Bohomol <8773927@gmail.com>
2023-10-05 20:38:51 +0300

    - added create mixin to `BorrowingViewSet` to enable creation endpoint - also added perform_create method to attach current user to the borrowing

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,14 +1,27 @@
 from rest_framework import mixins
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import GenericViewSet
 
 from borrowings.models import Borrowing
-from borrowings.serializers import BorrowingSerializer
+from borrowings.serializers import BorrowingSerializer, BorrowingListDetailSerializer, BorrowingCreateSerializer
 
 
 class BorrowingViewSet(
     mixins.ListModelMixin,
     mixins.RetrieveModelMixin,
+    mixins.CreateModelMixin,
     GenericViewSet
 ):
     queryset = Borrowing.objects.all()
     serializer_class = BorrowingSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_serializer_class(self):
+        if self.action in ("list", "retrieve"):
+            return BorrowingListDetailSerializer
+        if self.action == "create":
+            return BorrowingCreateSerializer
+        return BorrowingSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
