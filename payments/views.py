aacf099d84a95c07072a6da22fbf6fe1729f96b7
Volodymyr Bohomol <8773927@gmail.com>
2023-10-09 19:58:01 +0300

    - created PaymentViewSet with list and retrieve functionality

--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import mixins
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import GenericViewSet
 
-# Create your views here.
+from payments.models import Payment
+from payments.serializers import PaymentSerializer
+
+
+class PaymentViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    GenericViewSet,
+):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        queryset = self.queryset
+        if not self.request.user.is_staff:
+            queryset = queryset.filter(borrowing__user__id=self.request.user.id)
+        return queryset.distinct()
