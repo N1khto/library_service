98fb7f0321fd2bf3f2957390e0088930edbcd3c5
Volodymyr Bohomol <8773927@gmail.com>
2023-10-09 16:48:25 +0300

    - added payment model

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+from borrowings.models import Borrowing
+
+
+class Payment(models.Model):
+    class StatusChoices(models.TextChoices):
+        PENDING = "pending"
+        PAID = "paid"
+
+    class TypeChoices(models.TextChoices):
+        PAYMENT = "payment"
+        FINE = "fine"
+
+    status = models.CharField(max_length=15, choices=StatusChoices.choices)
+    type = models.CharField(max_length=15, choices=TypeChoices.choices)
+    borrowing = models.ForeignKey(Borrowing, on_delete=models.CASCADE)
+    session_url = models.URLField(max_length=255)
+    session_id = models.CharField(max_length=255)
+    money_to_pay = models.DecimalField(max_digits=10, decimal_places=2)
