522171b2ba54859d1230855a437e89535db23a85
Volodymyr Bohomol <8773927@gmail.com>
2023-10-12 11:15:00 +0300

    - added StripeCheckoutView for creation new stripe session - added endpoint for it to urls

--- payments/urls.py
+++ payments/urls.py
@@ -1,10 +1,14 @@
+from django.urls import path, include
 from rest_framework import routers
 
-from payments.views import PaymentViewSet
+from payments.views import PaymentViewSet, StripeCheckoutView
 
 router = routers.DefaultRouter()
 router.register("", PaymentViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    path("", include(router.urls)),
+    path("create-checkout", StripeCheckoutView.as_view(), name="create_checkout")
+]
 
 app_name = "payments"
--- payments/views.py
+++ payments/views.py
@@ -1,9 +1,18 @@
-from rest_framework import mixins
+import stripe
+from django.shortcuts import redirect
+
+from rest_framework import mixins, status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.viewsets import GenericViewSet
+from django.conf import settings
 
 from payments.models import Payment
 from payments.serializers import PaymentSerializer
+
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
 
 
 class PaymentViewSet(
@@ -20,3 +29,26 @@ class PaymentViewSet(
         if not self.request.user.is_staff:
             queryset = queryset.filter(borrowing__user__id=self.request.user.id)
         return queryset.distinct()
+
+
+class StripeCheckoutView(APIView):
+    def post(self, request):
+        try:
+            checkout_session = stripe.checkout.Session.create(
+                line_items=[
+                    {
+                        'price': 'price_1O0JC0F3PkC8kkFzwpSLHv6g',
+                        'quantity': 1,
+                    },
+                ],
+                mode='payment',
+                success_url="http://127.0.0.1:8000/api/borrowings" + '/?success=true',
+                cancel_url="http://127.0.0.1:8000/api/borrowings" + '/?cancelled=true',
+            )
+            return redirect(checkout_session.url)
+        except Exception as e:
+            return Response(
+                {"error": "stripe session error"},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+
